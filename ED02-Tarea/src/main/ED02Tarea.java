c46997ab48d9681acffbf11026b97576334a948e
AdryHD <152119270+AdryHD@users.noreply.github.com>
2024-09-26 20:07:06 -0600

    Adry
    Numero invertido

--- ED02-Tarea/src/main/ED02Tarea.java
+++ ED02-Tarea/src/main/ED02Tarea.java
@@ -1,21 +1,42 @@
 package main;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
-/**
- *
- * @author adryhd
- */
 public class ED02Tarea {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+public static void main(String[] args) {
+    int numero = 852499;
+    System.out.print("Número invertido (recursivo): ");
+    invierte(numero);  // Llamada al método recursivo
+    System.out.println();  // Para nueva línea después del resultado 
+    invierteIterativo(numero);  // Llamada al método iterativo
 }
+
+    public static void invierte(int numero) {
+    // Condición de parada: si el número es menor a 10, se imprime tal cual
+    if (numero < 10) {
+        System.out.print(numero);  // Imprimir el último dígito
+    } else {
+        // Imprimir el último dígito del número
+        System.out.print(numero % 10);
+        // Llamada recursiva con el número sin el último dígito
+        invierte(numero / 10);
+    }
+}
+
+public static void invierteIterativo(int numeroiterativo) {
+    System.out.print("Número invertido (iterativo): ");
+    
+  // bucle mientras el número sea mayor a 0
+    while (numeroiterativo > 0) {
+        // Imprimir el último dígito del número
+        System.out.print(numeroiterativo % 10);
+        // Eliminar el último dígito del número
+        numeroiterativo = numeroiterativo / 10;
+    }
+    System.out.println();  // Para nueva línea después del resultado
+}
+
+
+}
+
+
