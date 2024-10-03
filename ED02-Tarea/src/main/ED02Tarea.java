3ce90115ec55f12393f7e141a8be938e3009c526
AdryHD <152119270+AdryHD@users.noreply.github.com>
2024-10-02 18:19:28 -0600

    adry
    prueba

--- ED02-Tarea/src/main/ED02Tarea.java
+++ ED02-Tarea/src/main/ED02Tarea.java
@@ -31,14 +31,47 @@ public static void invierteIterativo(int numeroiterat) {
     
   // bucle mientras el num sea mayor a 0
     while (numeroiterat > 0) {
-        // Imprimir el último digiyo 
+        // Imprimir ultimo num 
         System.out.print(numeroiterat % 10);
-        // Elimina el último digiyo 
+        // Elimina ultimo numero 
         numeroiterat = numeroiterat / 10;
     }
     System.out.println();  // nueva linea
 }
 
+public class Busqueda {
+    // Arreglo ordenado de enteros
+    private int[] v;
+
+    // Constructor que inicializa el arreglo
+    public Busqueda(int[] v) {
+        this.v = v;
+    }
+
+    // Método público que inicia la búsqueda
+    public int busca(int x) {
+        return buscaR(x, 0, v.length - 1);  // Llamada al método recursivo
+    }
+
+    // Método privado que realiza la búsqueda recursiva
+    private int buscaR(int x, int izquierda, int derecha) {
+        if (izquierda > derecha) {
+            return -1;  // Valor no encontrado
+        }
+        
+        int mitad = (izquierda + derecha) / 2;
+
+        if (v[mitad] == x) {
+            return mitad;  // Valor encontrado
+        } else if (v[mitad] < x) {
+            // Buscar en la mitad derecha
+            return buscaR(x, mitad + 1, derecha);
+        } else {
+            // Buscar en la mitad izquierda
+            return buscaR(x, izquierda, mitad - 1);
+        }
+    }
+}
 
 }
 
